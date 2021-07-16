65e98b62be7ffd8cd13d7b50dacc3330fac52512
Damiano-F <damiano.fontanella@studio.unibo.it>
2021-07-16 21:34:08 +0200

    added first maximum total flow in layouts

--- layout.py
+++ layout.py
@@ -1,4 +1,5 @@
 from dfops import cross_sum
+from dictops import maximum_keys
 
 def layouts(from_to):
 
@@ -9,3 +10,9 @@ def layouts(from_to):
             tot_flow = cross_sum(el, idx, idx, False)
             tot_flows.update({idx: tot_flow})
         flows_list.append(tot_flows)
+
+    max_cells = []
+    for el in flows_list:
+        max = maximum_keys(el)
+        max_cells.append(max)
+    print(max_cells)
